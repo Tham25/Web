da887ba127ad9f73d12868d1bccddd7d8dffb3e7
luuthihongtham <luuthihongtham@ginno.com>
2022-04-15 17:57:21 +0700

    App TodoList use lib UI

---
+++ AppTodoList/component/App.js
@@ -0,0 +1,22 @@
+import html from '../core.js';
+// import {connect} from '../store.js'
+import Header from '../component/Header.js';
+import TodoList from '../component/TodoList.js';
+import Footer from '../component/Footer.js';
+
+
+// const connector = connect();
+
+function App() {
+
+    return html`
+        <section class="todoapp">
+            ${Header()}
+            ${TodoList()}
+            ${Footer()}
+        </section>
+    `
+}
+
+// export default connector(App);
+export default App;
