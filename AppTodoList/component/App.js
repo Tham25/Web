19635c344d13f1a6e8d99ac1c8ee9dbea75d65f4
luuthihongtham <luuthihongtham@ginno.com>
2022-04-19 14:31:10 +0700

    completed app todolist

--- AppTodoList/component/App.js
+++ AppTodoList/component/App.js
@@ -1,22 +1,19 @@
 import html from '../core.js';
-// import {connect} from '../store.js'
+import {connect} from '../store.js'
 import Header from '../component/Header.js';
 import TodoList from '../component/TodoList.js';
 import Footer from '../component/Footer.js';
 
 
-// const connector = connect();
-
-function App() {
+function App({todos}) {
 
     return html`
         <section class="todoapp">
             ${Header()}
-            ${TodoList()}
-            ${Footer()}
+            ${todos.length > 0 && TodoList()}
+            ${todos.length > 0 && Footer()}
         </section>
     `
 }
 
-// export default connector(App);
-export default App;
+export default connect()(App);
--- AppTodoList/logger.js
+++ AppTodoList/logger.js
@@ -1,3 +1,14 @@
 export default function logger(reducer) {
-    
+
+    return (prevState, action, args) => {
+        console.group(action);
+        console.log('Prev State:', prevState);
+        console.log('Action arguments:', args);
+
+        const nextState = reducer(prevState, action, args);
+        console.log('nextState:', nextState);
+
+        console.groupEnd(action);
+        return nextState;
+    }
 }
